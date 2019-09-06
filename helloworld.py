8bb6be68a50cd4fa30e591a23513d1af4b909901
brianlin <brianlin@umich.edu>
2019-09-06 16:08:24 -0400

    Initial Commit - helloworld.py
    Created helloworld.py program to print name, two rand numbers in [0,100]
    and prints their sum and averages

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+import random
+
+print("Brian")
+num1 = random.randint(0,101)
+num2 = random.randint(0,101)
+print(num1)
+print(num2)
+print("Sum  = ")
+print(num1 + num2)
+print("Average = ")
+print((num1 + num2)/2)
+
+
